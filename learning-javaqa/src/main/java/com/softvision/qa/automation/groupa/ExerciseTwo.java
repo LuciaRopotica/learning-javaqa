a4ee7c4774e5db826b9080b49846cb32f32d3259
Lucia Ropotica <lucia.ropotica@softvision.ro>
2019-10-10 17:35:07 +0300

    Exercise02

--- learning-javaqa/src/main/java/com/softvision/qa/automation/groupa/ExerciseTwo.java
+++ learning-javaqa/src/main/java/com/softvision/qa/automation/groupa/ExerciseTwo.java
@@ -1,23 +1,42 @@
 package com.softvision.qa.automation.groupa;
 
+import java.util.Scanner;
+
 public class ExerciseTwo {
+    //Sa se implementeze o metoda care primeste ca parametru un numar natural N mai mare ca zero.
+    // Sa se afiseze suma numerelor pare cuprinse intre zero si N,
+    // si media numerelor impare cuprinse intre zero si N.
+    static int n = 0;
+    static int counterImpare = 0;
+    static Double sumaPare = 0.0;
+    static Double sumaImpare = 0.0;
+    static Double mediaAritmetica = 0.0;
+
     public static void main(String[] args) {
-        int se = sumEven(10);
-        System.out.println(se);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Da o valoare lui n:");
+        n = scan.nextInt();
+        Calcul();
+        scan.close();
     }
 
-    public static void printCount(int n) {
-        for (int i = 0; i <= n; i = i + 2) {
-            System.out.println(i);
-        }
-    }
+    public static void Calcul() {
+        if (n < 0) {
+            System.out.println("Valoarea lui n nu poate fi mai mica de 0.");
+        } else {
+            for (int i = 0; i <= n; i++) {
+                if (i % 2 == 0) {
+                    sumaPare = sumaPare + i;
+                } else {
+                    counterImpare = counterImpare + 1;
+                    sumaImpare = sumaImpare + i;
+                }
+            }
+            mediaAritmetica = sumaImpare / counterImpare;
+            System.out.println("Suma numerelor pare cuprinse intre 0 si " + n + " este egala cu " + sumaPare);
+            System.out.println("Media aritmetica a numerelor impare cuprinse intre 0 si " + n + " este egala cu " + mediaAritmetica);
 
-    public static int sumEven(int n) {
-        int s = 0;
-        for (int i = 0; i <= n; i = i + 2) {
-            s = s + i;
         }
-        return s;
     }
 }
 
